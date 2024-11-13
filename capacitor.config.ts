b81573759a7daecde60d06eab5ceab1005a2ec24
calistaandt <calistaandta@gmail.com>
2024-11-13 14:49:49 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'prakmobile-firebase',
+  webDir: 'dist'
+};
+
+export default config;
